9d7dae4dde32a675abc322372a07fe587ebf3b1a
Divine Hycenth <divinehycenth@outlook.com>
2020-07-14 21:33:41 +0100

    🍌 removed serviceWorker.js file

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './scss/base.scss';
 
 ReactDOM.render(
@@ -12,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
