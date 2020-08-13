585973b21be9be3ac850b677ae8622280c1229e6
Divine Hycenth <divinehycenth@outlook.com>
2020-08-13 21:17:23 +0100

    Added .eslintrc package.json packages/alert/node_modules/@nature-ui/utils packages/alert/package.json packages/alert/src/index.tsx packages/alert/src/style.css packages/icon/node_modules/@nature-ui/utils packages/icon/package.json packages/system/node_modules/@nature-ui/utils packages/system/package.json packages/utils/package.json packages/utils/src/clx.ts yarn.lock.

--- packages/alert/src/index.tsx
+++ packages/alert/src/index.tsx
@@ -1,5 +1,70 @@
 import * as React from 'react';
+import { clx } from '@nature-ui/utils';
 
-const alert: React.FC = () => <h1>Hello World! Alert</h1>;
+export const ALERT_STATUSES = {
+  error: {
+    colorScheme: 'red',
+    icon: '',
+  },
+  info: {
+    colorScheme: 'blue',
+    icon: '',
+  },
+  success: {
+    colorScheme: 'green',
+    icon: '',
+  },
+  warning: {
+    colorScheme: 'orange',
+    icon: '',
+  },
+};
+
+interface AlertOptions {
+  /**
+   * The status of the alert
+   */
+  status?: keyof typeof ALERT_STATUSES;
+  /**
+   * The variant of the alert style to use
+   */
+  variant?: string;
+
+  /**
+   * Other tailwind utility classes or custom classnames you wish to include
+   */
+  className?: string;
+}
+
+export type AlertProps = AlertOptions;
+
+const DEFAULT_CLASSES = 'px-4 py-3';
+const DANGER_CLASS = 'bg-red-300';
+
+/*
+ *clx(BASE_BUTTON, {
+ *    [className]: className,
+ *    [CONTAINED_BUTTON]: !outlined,
+ *    [OUTLINED_BUTTON]: outlined,
+ *  })
+ *
+ */
+
+const alert: React.ForwardRefExoticComponent<AlertProps> = React.forwardRef(
+  (properties: AlertProps, ref: React.Ref<any>) => {
+    const { className = 'ml-12' } = properties;
+
+    const classes = clx(DEFAULT_CLASSES, {
+      [className]: className,
+      [DANGER_CLASS]: 'danger',
+    });
+
+    return (
+      <div ref={ref} className={classes}>
+        Your browser is outdated! Your Nature experience may be degraded.
+      </div>
+    );
+  }
+);
 
 export default alert;
--- packages/utils/src/clx.ts
+++ packages/utils/src/clx.ts
@@ -1,12 +1,9 @@
 type Other = string | number | null | boolean | undefined;
-export type ClassValue = ClassArray | ClassDict | Other;
+type ClassValue = ClassArray | ClassDict | Other;
 
 interface ClassDict {
   [key: string]: Other;
 }
-export type ClassArray = Array<ClassValue>;
+type ClassArray = Array<ClassValue>;
 
-const clx = (...classNames: ClassValue[]): string =>
-  classNames.filter(Boolean).join(' ');
-
-export default clx;
+export const clx = (...classNames: ClassValue[]): string => classNames.filter(Boolean).join(' ');
