b7e3f261cd5da443d6a51aed1f62b6a0d7055fba
Divine Hycenth <divinehycenth@outlook.com>
2020-08-15 13:07:39 +0100

    feat: 🚀 built system module

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,1 +1,5 @@
-export * from './clx';
+export * from './system';
+export * from './should-forward-prop';
+export * from './utils/style';
+export * from './typings/system.types';
+export * from './typings/types';
