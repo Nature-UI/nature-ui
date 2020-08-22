53d457b44b5c07c10c0e4a4b2b71b53bd68f0768
Divine Hycenth <divinehycenth@outlook.com>
2020-08-22 09:48:30 +0100

    fix: 🌴 changed @chakra-ui/icon to @nature-ui/icon

--- packages/icons/src/Minus.tsx
+++ packages/icons/src/Minus.tsx
@@ -1,11 +1,11 @@
-import { createIcon } from "@chakra-ui/icon"
-import * as React from "react"
+import { createIcon } from '@nature-ui/icon';
+import * as React from 'react';
 
 export const MinusIcon = createIcon({
-  displayName: "MinusIcon",
+  displayName: 'MinusIcon',
   path: (
-    <g fill="currentColor">
-      <rect height="4" width="20" x="2" y="10" />
+    <g fill='currentColor'>
+      <rect height='4' width='20' x='2' y='10' />
     </g>
   ),
-})
+});
