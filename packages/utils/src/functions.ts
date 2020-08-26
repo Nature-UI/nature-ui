9940acb1c7a337bd9255ec2c16a81c1ac6707d95
Divine Hycenth <divinehycenth@outlook.com>
2020-08-26 18:43:51 +0100

    feat: ⛰  created darken and lighten variants for colors

--- packages/utils/src/functions.ts
+++ packages/utils/src/functions.ts
@@ -39,3 +39,34 @@ export const once = (fn?: Function | null): any => {
     return result;
   };
 };
+
+/**
+ *
+ * @param value a valid tailwindcss color
+ * Avoid specifying where the color is going to be applied
+ * @example `blue-400`, `orange-200` is a valid color
+ * things like `bg-red-500`, `text-gray-100`... are invalid values.
+ */
+
+export const darken = (value: string): string => {
+  const splitStr = value.split('-');
+
+  if (splitStr.length > 2) {
+    throw new Error(`${value} is an invalid type`);
+  }
+
+  const dept = Number(splitStr[1]);
+  const color = splitStr[0];
+
+  if (dept >= 800) {
+    return `${color}-${dept - 200}`;
+  }
+
+  return `${color}-${dept + 200}`;
+};
+
+export const lighten = (value: string, amount = 100): string => {
+  const color = value.split('-')[0];
+
+  return `${color}-${amount}`;
+};
