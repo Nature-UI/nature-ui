407c279db0e2f9205637022f253d9f7a14c3214c
Divine Hycenth <divinehycenth@outlook.com>
2020-08-29 11:07:12 +0100

    feat: ⛹  added objects function

---
+++ packages/utils/src/objects.ts
@@ -0,0 +1,105 @@
+import * as merge from 'deepmerge';
+
+import { Omit, Dict } from './types';
+
+export const omit = <T extends Dict, K extends keyof T>(
+  object: T,
+  keys: K[]
+) => {
+  const result: Dict = {};
+
+  for (const key in object) {
+    if (!keys.includes(key as any)) {
+      result[key] = object[key];
+    }
+  }
+
+  return result as Omit<T, K>;
+};
+
+export const pick = <T extends Dict, K extends keyof T>(
+  object: T,
+  keys: K[]
+) => {
+  const result = {} as { [P in K]: T[P] };
+
+  for (const key of keys) {
+    if (key in object) {
+      result[key] = object[key];
+    }
+  }
+
+  return result;
+};
+
+export const split = <T extends Dict, K extends keyof T>(
+  object: T,
+  keys: K[]
+) => {
+  const picked: Dict = {};
+  const omitted: Dict = {};
+
+  for (const key in object) {
+    if (keys.includes(key as T[K])) {
+      picked[key] = object[key];
+    } else {
+      omitted[key] = object[key];
+    }
+  }
+
+  return [picked, omitted] as [{ [P in K]: T[P] }, Omit<T, K>];
+};
+
+/**
+ * Get value from a deeply nested object using a string path
+ * @param obj - the object
+ * @param path - the string path
+ * @param def  - the fallback value
+ */
+
+export const get = (
+  obj: any,
+  path: string | number,
+  fallback?: any,
+  index?: number
+) => {
+  // @ts-ignore
+  path = (path?.split?.('.') ?? [path]) as string;
+
+  for (index = 0; index < path.length; index++) {
+    obj = obj ? obj[path[index]] : undefined;
+  }
+
+  const result = (obj = undefined ? fallback : obj);
+
+  return result;
+};
+
+/**
+ * Get value from deeply nested object, based on path
+ * It returns the path value if not found in object
+ *
+ * @param path - the string path or value
+ * @param scale - the string path or value
+ */
+
+export const getWithDefault = (path: any, scale: any) => {
+  return get(scale, path, path);
+};
+
+export { merge };
+
+export const filterUndefined = (object: Dict) => {
+  const result = { ...object };
+
+  for (const item in result) {
+    if (typeof result[item] === 'undefined') {
+      delete result[item];
+    }
+  }
+
+  return result;
+};
+
+export const objectKeys = <T extends Dict>(obj: T) =>
+  (Object.keys(obj) as unknown) as (keyof T)[];
