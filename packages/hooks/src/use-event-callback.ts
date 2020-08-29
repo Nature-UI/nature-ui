00c26271c800e8fd74c214386cafc564936dc608
Divine Hycenth <divinehycenth@outlook.com>
2020-08-29 20:29:19 +0100

    feat: ⛹  created use-event-callback hook

---
+++ packages/hooks/src/use-event-callback.ts
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useSafeLayoutEffect } from './use-safe-layout-effect';
+
+/**
+ * React hook for performant `useCallbacks`
+ *
+ * @see https://github.com/facebook/react/issues/14099#issuecomment-440013892
+ */
+
+export const useEventCallback = <T extends Event | React.SyntheticEvent>(
+  callback: (event: T, ...args: any[]) => void
+) => {
+  const ref = React.useRef(callback);
+  useSafeLayoutEffect(() => {
+    ref.current = callback;
+  });
+
+  return React.useCallback(
+    (event: T, ...args: any[]) => ref.current(event, ...args),
+    []
+  );
+};
