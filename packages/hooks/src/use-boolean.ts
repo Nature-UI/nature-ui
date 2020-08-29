ff7e117443af76eb36935dfe184d47b348a0412b
Divine Hycenth <divinehycenth@outlook.com>
2020-08-29 19:13:54 +0100

    feat: ⛹  created useBoolean hook

---
+++ packages/hooks/src/use-boolean.ts
@@ -0,0 +1,27 @@
+import { useCallback, useState } from 'react';
+
+type InitialState = boolean | (() => boolean);
+
+/**
+ * React hook to manage boolean (on - off) states
+ *
+ * @param initialState the initial boolean state value
+ */
+
+export const useBoolean = (initialState: InitialState = false) => {
+  const [value, setValue] = useState(initialState);
+
+  const on = useCallback(() => {
+    setValue(true);
+  }, []);
+
+  const off = useCallback(() => {
+    setValue(false);
+  }, []);
+
+  const toggle = useCallback(() => {
+    setValue((prev) => !prev);
+  }, []);
+
+  return [value, { on, off, toggle }] as const;
+};
