9d6dfd92c765afa80c23e052bddf09004788a391
Divine Hycenth <divinehycenth@outlook.com>
2020-09-16 12:10:21 +0100

    feat: ⛹  finished avatar badge component

---
+++ packages/avatar/src/avatar-group.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+/** @jsx jsx */
+import {jsx, nature, PropsOf} from '@nature-ui/system'
+import { __DEV__ } from '@nature-ui/utils';
+
+const AvatarExcessLabel = (props: PropsOf<"span">) => {
+    return <nature.span className />
+
+}
+
+
+if (__DEV__) {
+    AvatarExcessLabel.displayName = "AvatarExcessLabel"
+  }
+  
+interface AvatarGroupOptions {
+
+}
