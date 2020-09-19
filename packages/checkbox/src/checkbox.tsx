bd12a26414db54ef1b955f671d5822de627b5dfc
Divine Hycenth <divinehycenth@outlook.com>
2020-09-19 17:30:10 +0100

    feat: 🍒 created checkbox component

---
+++ packages/checkbox/src/checkbox.tsx
@@ -0,0 +1,34 @@
+/** @jsx jsx*/
+import { forwardRef, nature, PropsOf, jsx } from '@nature-ui/system';
+import { css } from 'emotion';
+import * as React from 'react';
+
+const transition = css`
+  transition: transform 240ms, opacity 240mx;
+`;
+
+const StyledControl = forwardRef<PropsOf<typeof nature.div>>((props, ref) => {
+  const _className = `inline-flex items-center justify-center align-top select-none flex-shrink-0 ${transition}`;
+  return <nature.div className={_className} ref={ref} {...props} />;
+});
+
+const StyledLabel = forwardRef<PropsOf<typeof nature.div>>((props, ref) => {
+  const _className = `select-none`;
+  return <nature.div className={_className} ref={ref} {...props} />;
+});
+
+const StyledWrapper = forwardRef<PropsOf<typeof nature.label>>((props, ref) => {
+  const _className = `cursor-pointer inline-flex items-center align-top relative ${transition}`;
+  return <nature.label className={_className} ref={ref} {...props} />;
+});
+
+type BaseControlProps = Omit<
+  PropsOf<typeof StyledControl>,
+  'onChange' | 'defaultChecked'
+>;
+
+type Omitted = 'checked' | 'defaultChecked';
+
+export type CheckboxProps = BaseControlProps &
+  Omit<PropsOf<'input'>, Omitted> &
+  UseCh;
