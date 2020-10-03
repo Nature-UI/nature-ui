c8b4c4571494037393c976af3a1f7adf2dc6ae9f
Divine Hycenth <divinehycenth@outlook.com>
2020-10-03 15:57:26 +0100

    feat: 🍒 created scale-fade-transition stories

--- packages/transition/stories/scale-fade.stories.tsx
+++ packages/transition/stories/scale-fade.stories.tsx
@@ -8,8 +8,28 @@ export default {
   title: 'Transition/ScaleFade',
   component: ScaleFade,
 } as Meta;
+export function Basic() {
+  const [isOpen, setIsOpen] = React.useState(true);
+  return (
+    <>
+      <button onClick={() => setIsOpen((p) => !p)}>Click Me</button>
+      <ScaleFade in={isOpen}>
+        {(styles) => (
+          <Modal
+            style={{
+              ...styles,
+              transform: styles.transform
+                ? `${modalStyles.transform} ${styles.transform}`
+                : `${modalStyles.transform}`,
+            }}
+          />
+        )}
+      </ScaleFade>
+    </>
+  );
+}
 
-export function ScaleFadeExample() {
+export function ScaleFadeWithTimeout() {
   const [isOpen, setIsOpen] = React.useState(true);
   return (
     <>
