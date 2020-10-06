d3e63975402cc3bbb54cdf408e9bc1a77dbbe42a
Divine Hycenth <divinehycenth@outlook.com>
2020-10-06 21:46:20 +0100

    feat: 🍒 added text-area example

--- packages/form-control/src/form-control.tsx
+++ packages/form-control/src/form-control.tsx
@@ -183,10 +183,14 @@ export type FormLabelProps = PropsOf<typeof StyledLabel>;
  */
 export const FormLabel = React.forwardRef(
   (props: FormLabelProps, ref: React.Ref<any>) => {
-    const { className, ...rest } = props;
+    const { className = '', ...rest } = props;
+
+    const _className = clsx(`mb-2`, {
+      [className]: className,
+    });
     const ownProps = useFormControlLabel(rest);
 
-    return <StyledLabel ref={ref} className={className} {...ownProps} />;
+    return <StyledLabel ref={ref} className={_className} {...ownProps} />;
   }
 );
 
@@ -246,6 +250,7 @@ export type HelpTextProps = PropsOf<typeof StyledHelperText>;
 export const FormHelperText = React.forwardRef(
   (props: HelpTextProps, ref: React.Ref<any>) => {
     const field = useFormControlContext();
+    const { className = '', ...rest } = props;
 
     /**
      * Notify the field context when the help text is rendered on
@@ -257,11 +262,16 @@ export const FormHelperText = React.forwardRef(
       return () => field?.setHasHelpText.off();
     }, []);
 
+    const _className = clsx(`mt-2 text-sm text-gray-600`, {
+      [className]: className,
+    });
+
     return (
       <StyledHelperText
+        className={_className}
         ref={ref}
         id={props.id ?? field?.helptextId}
-        {...props}
+        {...rest}
       />
     );
   }
@@ -292,15 +302,21 @@ export type FormErrorMessageProps = PropsOf<typeof StyledErrorText>;
  */
 export const FormErrorMessage = React.forwardRef(
   (props: FormErrorMessageProps, ref: React.Ref<any>) => {
+    const { className = '', ...rest } = props;
     const field = useFormControlContext();
 
     if (!field?.isInvalid) return null;
 
+    const _className = clsx(`text-sm mt-2  text-red-600`, {
+      [className]: className,
+    });
+
     return (
       <StyledErrorText
+        className={_className}
         ref={ref}
         id={props.id ?? field?.feedbackId}
-        {...props}
+        {...rest}
       />
     );
   }
@@ -319,9 +335,19 @@ export const FormErrorIcon = React.forwardRef(
     const field = useFormControlContext();
 
     if (!field?.isInvalid) return null;
+    const { className = '', ...rest } = props;
+    const _className = clsx(`mr-2 text-red-600`, {
+      [className]: className,
+    });
 
     return (
-      <Icon ref={ref} aria-hidden {...props}>
+      <Icon
+        boxSize='1rem'
+        className={_className}
+        ref={ref}
+        aria-hidden
+        {...rest}
+      >
         <path
           fill='currentColor'
           d='M11.983,0a12.206,12.206,0,0,0-8.51,3.653A11.8,11.8,0,0,0,0,12.207,11.779,11.779,0,0,0,11.8,24h.214A12.111,12.111,0,0,0,24,11.791h0A11.766,11.766,0,0,0,11.983,0ZM10.5,16.542a1.476,1.476,0,0,1,1.449-1.53h.027a1.527,1.527,0,0,1,1.523,1.47,1.475,1.475,0,0,1-1.449,1.53h-.027A1.529,1.529,0,0,1,10.5,16.542ZM11,12.5v-6a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Z'
--- packages/form-control/stories/form-control.stories.tsx
+++ packages/form-control/stories/form-control.stories.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
-import { jsx, nature, PropsOf } from '@nature-ui/system';
+import { jsx, nature, PropsOf, css, clsx } from '@nature-ui/system';
 import * as React from 'react';
+import { Meta } from '@storybook/react';
 
 import {
   FormControlOptions,
@@ -10,14 +11,17 @@ import {
   FormHelperText,
   FormLabel,
   useFormControl,
+  FormErrorIcon,
 } from '../src';
 
 export default {
   title: 'FormControl',
   component: FormControl,
-};
+  decorators: [(story: Function) => <div className='max-w-md'>{story()}</div>],
+} as Meta;
 
 const InputTag = nature('input');
+const TextareaTag = nature('textarea');
 
 type OmittedTypes = 'disabled' | 'required' | 'readOnly' | 'size';
 
@@ -37,8 +41,21 @@ const StyledInput = (props: PropsOf<typeof InputTag> & Props) => (
 const Input = React.forwardRef(
   (props: InputProps, ref: React.Ref<HTMLInputElement>) => {
     const inputProps = useFormControl<HTMLInputElement>(props);
+    const { className = '', ...rest } = props;
+    const { 'aria-invalid': isInvalid } = inputProps;
 
-    return <StyledInput ref={ref} {...inputProps} />;
+    const _className = clsx(
+      `h-10 px-4 rounded border-2 border-solid outline-none w-full`,
+      {
+        [className]: className,
+        'border-red-600 focus:border-red-600': isInvalid,
+        'focus:border-blue-400': !isInvalid,
+      }
+    );
+
+    return (
+      <StyledInput className={_className} ref={ref} {...inputProps} {...rest} />
+    );
   }
 );
 
@@ -48,5 +65,56 @@ export const InputExample = () => (
     <Input placeholder='First Name' />
     <FormHelperText>Keep it very short and sweet!</FormHelperText>
     <FormErrorMessage>Your First name is invalid</FormErrorMessage>
+  </FormControl>
+);
+
+type TextAreaProps = Omit<PropsOf<'textarea'>, OmittedTypes> &
+  FormControlOptions;
+
+const StyledTextArea = (props: TextAreaProps) => {
+  return (
+    <TextareaTag
+      css={{
+        padding: '8px',
+        minHeight: '80px',
+        lineHeight: 'short',
+      }}
+      {...props}
+    />
+  );
+};
+
+const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (props, ref) => {
+    const inputProps = useFormControl<HTMLTextAreaElement>(props);
+    const { className = '', ...rest } = props;
+
+    const { 'aria-invalid': isInvalid } = inputProps;
+
+    const _className = clsx(`w-full border-solid border-2 rounded`, {
+      [className]: className,
+      'border-red-600': isInvalid,
+    });
+
+    return (
+      <StyledTextArea
+        ref={ref}
+        className={_className}
+        {...inputProps}
+        {...rest}
+      />
+    );
+  }
+);
+
+export const TextAreaExample = () => (
+  <FormControl id='first-name' isInvalid>
+    <FormLabel>First name</FormLabel>
+    <TextArea placeholder='First Name' />
+    <FormHelperText>Keep it very short and sweet!</FormHelperText>
+    <FormErrorMessage>
+      <FormErrorIcon />
+      Your First name is invalid
+    </FormErrorMessage>
   </FormControl>
 );
