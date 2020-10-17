708cee7d915d0bd96fe335ce40fa112acd00f702
Divine Hycenth <divinehycenth@outlook.com>
2020-10-17 21:09:08 +0100

    feat: 🍒 finished input for now

--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -1,33 +1,55 @@
-import { FormControlOptions, useFormControl } from "@chakra-ui/form-control"
-import { chakra, forwardRef, PropsOf } from "@chakra-ui/system"
-import { cx, __DEV__ } from "@chakra-ui/utils"
-import * as React from "react"
+/** @jsx jsx */
+import { clsx, jsx, nature, PropsOf, css } from '@nature-ui/system';
+import { FormControlOptions, useFormControl } from '@nature-ui/form-control';
+import { __DEV__ } from '@nature-ui/utils';
+import * as React from 'react';
+
+const InputTag = nature('input');
 
 interface InputOptions {
   /**
    * The border color when the input is focused. Use color keys in `theme.colors`
    * @example
-   * focusBorderColor = "blue.500"
+   * focusBorderColor = "blue-500"
    */
-  focusBorderColor?: string
+  focusBorderColor?: string;
   /**
    * The border color when the input is invalid. Use color keys in `theme.colors`
    * @example
-   * errorBorderColor = "red.500"
+   * errorBorderColor = "red-500"
    */
-  errorBorderColor?: string
+  errorBorderColor?: string;
   /**
    * If `true`, the input element will span the full width of it's parent
    */
-  isFullWidth?: boolean
+  isFullWidth?: boolean;
 }
 
-type Omitted = "disabled" | "required" | "readOnly" | "size"
+type Omitted = 'disabled' | 'required' | 'readOnly' | 'size';
+
+const _SIZES = {
+  sm: {
+    height: '2rem',
+    fontSize: '0.875rem',
+    paddingLeft: '0.75rem',    paddingRight: '0.75rem',
+  },
+  md: {
+    height: '2.5rem',
+    fontSize: '1rem',
+    paddingLeft: '1rem',    paddingRight: '1rem',
+  },
+  lg: {
+    height: '3rem',
+    fontSize: '1.25rem',
+    paddingLeft: '1rem',    paddingRight: '1rem',
+  },
+};
 
 export interface InputProps
   extends Omit<PropsOf<typeof StyledInput>, Omitted>,
     FormControlOptions {
-  size?: string
+  size?: keyof typeof _SIZES | number;
+  variant?: "outlined" | "filled" | "flushed" | "unstyled";
 }
 
 /**
@@ -36,28 +58,43 @@ export interface InputProps
  * To style the input globally, change the styles in
  * `theme.components.Input`
  */
-
-const StyledInput = chakra<"input", InputOptions>("input", {
-  themeKey: "Input",
-  shouldForwardProp: (prop) =>
-    !["focusBorderColor", "errorBorderColor"].includes(prop),
-})
+const StyledInput = (props: PropsOf<typeof InputTag> & InputOptions) => {
+  return <InputTag {...props} />;
+};
 
 /**
  * Input
  *
  * Element that allows users enter single valued data.
  */
-export const Input = forwardRef<InputProps>(function Input(props, ref) {
-  const inputProps = useFormControl<HTMLInputElement>(props)
-  const _className = cx("chakra-input", props.className)
+export const Input = React.forwardRef(
+  (props: InputProps, ref: React.Ref<HTMLInputElement>) => {
+    const inputProps = useFormControl<HTMLInputElement>(props);
 
-  return <StyledInput ref={ref} {...inputProps} className={_className} />
-})
+    const { className = '', size = 'md', ...rest } = props;
+
+
+    const _css = typeof size === 'string' && css(_SIZES[size])
+    const _height = typeof size === 'number' && css({
+      height: size + 'px',
+    })
+
+    const _className = clsx(_css,`w-full rounded px-4 outline-none border border-solid border-gray-300 focus:shadow-outline transition-all duration-150`,  _height, className);
+
+    return (
+      <StyledInput
+        ref={ref}
+        {...inputProps}
+        {...rest}
+        className={_className}
+      />
+    );
+  }
+);
 
 if (__DEV__) {
-  Input.displayName = "Input"
+  Input.displayName = 'Input';
 }
 
-//@ts-ignore
-Input.__hidden = "Input"
+// @ts-ignore
+Input.__hidden = 'Input';
--- packages/input/stories/input.stories.tsx
+++ packages/input/stories/input.stories.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
+import {Story} from '@storybook/react';
+import { Stack } from "@nature-ui/layout"
 
-import { Input } from '../src';
+import { Input, InputProps } from '../src';
 
 export default {
   title: 'Input',
   component: Input,
 };
 
-export const Basic = () => <Input placeholder='Basic input' />;
+type InputType = Story<InputProps>
+
+const Template: InputType = args => <Input {...args} />
+
+export const Basic: InputType = Template.bind({})
+Basic.args = {
+  placeholder: "Basic input",
+}
+
+export const WithSizes = () => {
+  return (
+    <Stack spacing="1rem" >
+      {(["sm", "md", "lg", 45]).map((size) => (
+        // @ts-ignore
+        <Input size={size} placeholder="This is an input component"  key={size}/>
+      ))}
+    </Stack>
+  )
+}
+
