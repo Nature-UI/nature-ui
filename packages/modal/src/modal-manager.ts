8e989d61b9d8588dd91819f6ec46e9dd05fca13c
Divine Hycenth <divinehycenth@outlook.com>
2020-10-31 15:07:18 +0100

    feat: 🍒 created modal-manager

---
+++ packages/modal/src/modal-manager.ts
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+/**
+ * Proper state management for nested modals.
+ * Simplified, but inspired by material-ui's ModalManager class.
+ */
+export const manager = {
+  modals: [] as any,
+  add: function(modal: any){
+    this.modals.push(modal)
+  },
+  remove: function(modal: any){
+    this.modals = this.modals.filter((_modal: any) => _modal !== modal)
+  },
+  isTopModal(modal: any) {
+    const topmostModal = this.modals[this.modals.length - 1]
+    return topmostModal === modal
+  }
+}
+
+
+export function useModalManager(ref: React.Ref<any>, isOpen?: boolean) {
+  React.useEffect(() => {
+    isOpen && manager.add(ref)
+    return () => {
+      manager.remove(ref)
+    }
+  }, [isOpen, ref])
+}
