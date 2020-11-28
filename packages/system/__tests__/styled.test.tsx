c12f935b22a72fe4e64c8fcd13ce61d68f08fe71
Divine Hycenth <divinehycenth@outlook.com>
2020-11-28 20:45:30 +0100

    feat: ⚙ created system tests

--- packages/system/__tests__/styled.test.tsx
+++ packages/system/__tests__/styled.test.tsx
@@ -8,10 +8,62 @@ describe('@nature-ui/system', () => {
     const Div = nature('div');
     const tools = render(<Div as='span'>a</Div>);
 
-    expect(tools.getByAltText('a')).toMatchInlineSnapshot(`
+    expect(tools.getByText('a')).toMatchInlineSnapshot(`
     <span>
-    a
+      a
     </span>
+  `);
+  });
+
+  test('as component', () => {
+    type Props = {
+      isBig?: boolean;
+    };
+
+    const Toyota = ({ isBig, ...props }: Props) => (
+      <div data-size={isBig ? 'big' : 'small'} {...props} />
+    );
+
+    const Div = nature('div');
+
+    const tools = render(
+      <Div as={Toyota} isBig>
+        a
+      </Div>
+    );
+
+    expect(tools.getByText('a')).toMatchInlineSnapshot(`
+     <div
+       data-size="big"
+     >
+       a
+     </div>
+     `);
+  });
+
+  test('nature elements - renders with tailwind className props', () => {
+    const tools = render(<nature.div className='mt-40'>Welcome</nature.div>);
+
+    expect(tools.asFragment()).toMatchSnapshot();
+
+    expect(tools.getByText('Welcome')).toHaveClass(`mt-40`);
+  });
+
+  test('nature function - renders correctly', () => {
+    const Button = nature('button');
+    const tools = render(<Button>Click Me</Button>);
+
+    expect(tools.getByText('Click Me')).toMatchInlineSnapshot(`
+      <button>
+        Click Me
+      </button>
     `);
+  });
+
+  test('it allows pass through props', () => {
+    const Image = nature('img');
+    const tools = render(<Image data-testid='img' width='60px' />);
+
+    expect(tools.asFragment()).toMatchSnapshot();
   });
 });
