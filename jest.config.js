bd0156f81ff2dec4fb27a5b25bcfb0dae4553ebc
Divine Hycenth <divinehycenth@outlook.com>
2020-12-16 10:46:38 +0100

    fix: ✌ modified jest.config of each component

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: ['__tests__/**/*.{ts,tsx}'],
+  transform: { '.(ts|tsx)$': 'ts-jest/dist' },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  verbose: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
