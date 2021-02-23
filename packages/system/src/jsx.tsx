40053daf54046a72fd03668a0f9ee890725a5cf4
Divine Hycenth <divinehycenth@outlook.com>
2021-02-23 13:48:02 +0100

    fix:  ❌ temporary fix for emotion css prop

--- packages/system/src/jsx.tsx
+++ packages/system/src/jsx.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import { Dict } from '@nature-ui/utils';
-import { jsx as emotion } from '@emotion/react';
+import { jsx as emotion, Interpolation, Theme } from '@emotion/react';
 
 export const jsx = (
   type: React.ElementType = 'div',
   props: Dict,
   ...children: React.ReactNode[]
 ) => {
+  if (props.as) {
+    type = props.as;
+  }
   return emotion.apply(type, [type, props, ...children]);
 };
+
+declare module 'react' {
+  interface Attributes {
+    css?: Interpolation<Theme>;
+  }
+}
 
 export default jsx;
