223352cfe4172fa4d5d82aabc55d93b4cea49629
Divine Hycenth <divinehycenth@outlook.com>
2021-09-17 11:59:29 +0100

    fix: upgraded dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,21 @@
 module.exports = {
   stories: ['../packages/**/*.stories.@(ts|tsx|mdx)'],
   addons: [
-    '@storybook/preset-create-react-app',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-  ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [require.resolve('babel-preset-react-app')],
-          },
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
         },
-        require.resolve('react-docgen-typescript-loader'),
-      ],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  typescript: {
+    reactDocgen: false,
   },
 };
