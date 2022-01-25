4cf4653b89052d153cc9cb93ec08a07c0db82fc3
Divine Hycenth <divinehycenth@outlook.com>
2022-01-25 20:24:47 +0100

    chore: Upgrading storybook and webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,20 @@
 module.exports = {
   stories: ['../packages/**/*.stories.@(ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', {
+    name: '@storybook/addon-postcss',
+    options: {
+      cssLoaderOptions: {
+        importLoaders: 1
       },
-    },
-  ],
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
+    }
+  }],
   typescript: {
-    reactDocgen: false,
+    reactDocgen: false
   },
+  core: {
+    builder: "webpack5"
+  }
 };
