a13f25e0c6bd1530a71aaa0bf3e7b7864dcfa935
Divine Hycenth <divinehycenth@outlook.com>
2022-05-09 19:04:00 +0200

    fix: drawer stories button

--- packages/drawer/stories/drawer.stories.tsx
+++ packages/drawer/stories/drawer.stories.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@nature-ui/button';
-import * as React from 'react';
+import { Button, ButtonProps } from '@nature-ui/button';
+import React from 'react';
 import { Drawer, DrawerContent, DrawerOverlay } from '../src';
 
 export default {
@@ -7,17 +7,23 @@ export default {
   component: Drawer,
 };
 
+const StyledButton = (args: ButtonProps) => (
+  <Button
+    {...args}
+    className={`bg-gray-200 hover:bg-gray-300 text-gray-700 ${args.className}`}
+  />
+);
 export const Basic = () => {
   const [open, setOpen] = React.useState(false);
 
   return (
     <>
-      <Button onClick={() => setOpen(!open)}>Open</Button>
+      <StyledButton onClick={() => setOpen(!open)}>Open</StyledButton>
       <Drawer isOpen={open} onClose={() => setOpen(false)}>
         <DrawerOverlay />
         <DrawerContent>
           <div>This is the drawer content</div>
-          <Button>This is a Button</Button>
+          <StyledButton>This is a Button</StyledButton>
         </DrawerContent>
       </Drawer>
     </>
@@ -29,12 +35,12 @@ export const LeftPlacement = () => {
 
   return (
     <>
-      <Button onClick={() => setOpen(!open)}>Open</Button>
+      <StyledButton onClick={() => setOpen(!open)}>Open</StyledButton>
       <Drawer placement='left' isOpen={open} onClose={() => setOpen(false)}>
         <DrawerOverlay />
         <DrawerContent>
           <div>This is the drawer content</div>
-          <Button>This is a Button</Button>
+          <StyledButton>This is a Button</StyledButton>
         </DrawerContent>
       </Drawer>
     </>
