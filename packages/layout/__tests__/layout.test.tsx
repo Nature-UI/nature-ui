8972d2641c3310117b917e17d93e034491f0c9a3
Divine Hycenth <divinehycenth@outlook.com>
2022-06-30 11:18:33 +0200

    fix: layout tests

--- packages/layout/__tests__/layout.test.tsx
+++ packages/layout/__tests__/layout.test.tsx
@@ -1,40 +1,25 @@
-import { render } from '@nature-ui/test-utils';
+import { render, testA11y } from '@nature-ui/test-utils';
 import { Badge, Box } from '../src';
 
 describe('@nature-ui/layout', () => {
   describe('Box', () => {
-    test('should render box correctly', () => {
-      const tools = render(<Box>This is box</Box>);
-
-      expect(tools.asFragment()).toMatchSnapshot();
+    test('passes a11y test', async () => {
+      await testA11y(<Box>this is a box</Box>);
     });
-
     test('as - prop works correctly', () => {
-      const tools = render(
+      const { getByText } = render(
         <Box as='a' href='www.google.com'>
-          This is box
+          Box
         </Box>,
       );
-
-      expect(tools.asFragment()).toMatchSnapshot();
+      expect(getByText('Box').nodeName).toBe('A');
+      expect(getByText('Box')).toHaveAttribute('href');
     });
   });
 
   describe('Badge', () => {
-    test('should render with default theming', () => {
-      const tools = render(<Badge>Badge</Badge>);
-
-      expect(tools.asFragment()).toMatchSnapshot();
-    });
-
-    test('override the theming props', () => {
-      const tools = render(
-        <Badge variant='outline' color='pink-500'>
-          Badge
-        </Badge>,
-      );
-
-      expect(tools.asFragment()).toMatchSnapshot();
+    test('passes a11y test', async () => {
+      await testA11y(<Badge>this is a badge</Badge>);
     });
   });
 });
