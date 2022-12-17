b12706294eaa20d8d8010ed9765d41807580e789
Divine Hycenth <divinehycenth@outlook.com>
2022-12-17 07:08:25 +0100

    #212 alert component

--- packages/system/src/system.types.tsx
+++ packages/system/src/system.types.tsx
@@ -28,39 +28,6 @@ export type WithNature<Props> = Props extends { transition?: any }
   ? Props & Omit<NatureProps, 'transition'>
   : Props & NatureProps;
 
-/**
- * Regular component means:
- *
- * - Read the props of the component using the `PropsOf` utility
- * - Add Nature props to it using `withNature`
- * - Add the `as` prop. in this case, it doesn't do anything special.
- * - Return a JSX Element
- */
-// type RegularComponent<T extends As, P> = (
-//   props: WithNature<Omit<PropsOf<T>, 'size' | 'as' | keyof P>> &
-//     P & { as?: As },
-// ) => JSX.Element;
-
-/**
- * Extensible component means:
- *
- * - Read the props of the component using the `PropsOf` utility
- * - Use a typescript generic `TT` to store the component passed in the `as` prop
- * - Use the `WithAs` to merge the base component prop with `as` component prop
- * - Add Nature props to the resulting types.
- */
-// type ExtensibleComponent<T extends As, P> = <TT extends As = T>(
-//   props: WithNature<WithAs<PropsOf<T>, TT>> & P,
-// ) => JSX.Element;
-
-// type Comp<T extends As, P> = RegularComponent<T, P> | ExtensibleComponent<T, P>;
-
-// export type NatureComponent<T extends As, P extends Dict = {}> = Comp<T, P> & {
-//   displayName?: string;
-//   propTypes?: React.WeakValidationMap<Omit<PropsOf<T>, 'size'> & P>;
-//   defaultProps?: Partial<Omit<PropsOf<T>, 'size'> & P & NatureProps>;
-// };
-
 type Merge<T extends As, P> = P & Omit<PropsOf<T>, keyof P>;
 
 type Exotic<P> =
